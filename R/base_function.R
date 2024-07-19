abae73157afbd4c7da8a7ca4b4ecb1de34701299
spd <384220729@qq.com>
2024-07-19 14:52:36 +0800

    Update peakAnnotation function.

--- R/base_function.R
+++ R/base_function.R
@@ -320,3 +320,62 @@ getFeatureTable <- function(data, method = "medret", value = "intb"){
   featureTable <- dplyr::as_tibble(cbind(featureTable, featureValues))
   return(featureTable)
 }
+
+#' @title peakAnnotation.
+#'
+#' @param data A XcmsExperiment object.
+#' @param polarity positive or negative
+#' @param adinfo adinfo <- data(positive.adinfo, package = "cliqueMS")
+#' @param thread thread.
+#'
+#' @return A tibble.
+#' @export
+#'
+#' @examples
+#' data(positive.adinfo, package = "cliqueMS")
+#' positive.adinfo <- positive.adinfo[positive.adinfo$adduct %in%
+#'                                      c("[M+H]+", "[M+H-H2O]+", "[M+Na]+", "[M+H-NH3]+", "[M+K]+", "[M+NH4]+"),]
+#' resTable <- peakAnnotation(data = data_filter, polarity = "positive", adinfo = positive.adinfo, thread = length(data_filter))
+peakAnnotation <- function(data, polarity = "positive", adinfo, thread = 1){
+  #browser()
+  iterList <- lapply(1:length(data), function(i) {
+    data_n <- data[i]
+    attributes(data_n)$order <- i
+    return(data_n)
+  })
+  loop <- function(data_n){
+    data_xs <- xcms:::.XCMSnExp2xcmsSet(data_n)
+    set.seed(2)
+    ex.cliqueGroups <- cliqueMS::getCliques(data_xs, filter = TRUE, silent = FALSE)
+    ex.Isotopes <- cliqueMS::getIsotopes(ex.cliqueGroups, ppm = 10)
+    ex.Adducts <- cliqueMS::getAnnotation(ex.Isotopes, ppm = 10,
+                                          adinfo = adinfo, polarity = polarity,
+                                          normalizeScore = TRUE)
+    resTable <- ex.Adducts@peaklist[, c("mz", "rt", "maxo", "sample", "cliqueGroup", "isotope", "mass1", "an1")]
+    return(resTable)
+  }
+  pb <- utils::txtProgressBar(max = length(data), style = 3)
+  if(thread == 1){
+    resList <- lapply(1:length(iterList), function(i) {
+      utils::setTxtProgressBar(pb, i)
+      data_n <- iterList[[i]]
+      loop(i)
+    })
+  }else if(thread > 1){
+    cl <- snow::makeCluster(thread)
+    doSNOW::registerDoSNOW(cl)
+    opts <- list(progress = function(n) utils::setTxtProgressBar(pb,
+                                                                 n))
+    resList <- foreach::`%dopar%`(foreach::foreach(i = iterList,
+                                                   .packages = c("cliqueMS", "xcms"),
+                                                   .options.snow = opts),
+                                  {
+                                    loop(i)
+                                  })
+    snow::stopCluster(cl)
+    gc()
+  }else stop("Thread wrong!")
+  resTable <- purrr::list_rbind(resList)
+  resTable <- dplyr::as_tibble(resTable, rownames = "cpid")
+  return(resTable)
+}
